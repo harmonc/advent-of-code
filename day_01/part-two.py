a277760b0c0f4d9db3ed7ae8cf793e8ebf15fc21
Caleb Harmon <calebmharmon@gmail.com>
2024-12-01 14:45:44 -0600

    Day One Solutions

---
+++ day_01/part-two.py
@@ -0,0 +1,20 @@
+pairs = open('day_01/input.txt', "r").read().split('\n')
+listA = []
+mapB = {}
+
+for pair in pairs:
+    a,b = pair.split('   ')
+    listA.append(int(a))
+    if int(b) in mapB:
+        mapB[int(b)] = mapB[int(b)]+1
+    else:
+        mapB[int(b)] = 1
+
+listA.sort()
+result = 0
+
+for a in listA:
+    if a in mapB:
+        result += a * mapB[a]
+
+print(result)
